bd113102814736e934828fcd1fa6fc5fa1251452
Julie <jbosworth16@gmail.com>
2022-07-24 12:58:44 -0400

    Added functionality to get pokemon name from input

---
+++ code.js
@@ -0,0 +1,10 @@
+function create_accordion(){
+
+}
+function show_tooltext(){
+
+}
+function grab_pokemon_name(){
+    var pokemon_name = document.getElementById('find_pokemon').value
+    alert(pokemon_name)
+}
