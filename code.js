f7b4213ab1598c14099fef3f71f0ac7fa25679ee
Julie <jbosworth16@gmail.com>
2022-07-25 00:19:50 -0400

    Started to create code to make according for moves and buttons for types

--- code.js
+++ code.js
@@ -1,6 +1,26 @@
-function create_accordion(){
-
+function create_types(){
+    var body = document.getElementsByTagName("body")[0]
+    /*Change 5 to whatever the type # is from the query*/
+    for(var i = 0;i<1;i++){
+        var button = document.createElement("button")
+        button.className = "btn btn-light"
+        button.innerHTML = 'grass' /** This is the label on the button*/
 }
+
+function create_accordion(){
+    /*Change 5 to whatever the move set # is from the query*/
+    var body = document.getElementsByTagName("body")[0]
+    var start = document.getElementById('start')
+    for(var i = 0;i<5;i++){
+        var button = document.createElement("button")
+        button.id = 'accordion'
+        button.className = "btn btn-light"
+        button.innerHTML = 'tackle' /** This is the label on the button*/
+        start.parentNode.insertBefore(button,start)
+        body.appendChild(button)
+    }
+}
+
 function show_tooltext(){
 
 }
