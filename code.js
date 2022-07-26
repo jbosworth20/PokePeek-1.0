dd33e6f9bc42f3c7c2699490a3a0b7dc4a9bfc61
Julie <jbosworth16@gmail.com>
2022-07-26 12:49:51 -0400

    Updated how adding types work

--- code.js
+++ code.js
@@ -1,26 +1,22 @@
-function create_types(){ /** Need to think how to use this to determine background color maybe read 
-in the text and if = then we choose color? */
-    var body = document.getElementsByTagName("body")[0];
-    /*Change total_types to size of array containing types*/ 
-    var total_types = 1;
-    var name_of_type = 'Grass'
-    if(total_types = 1){
-        var type = document.createElement("button");
-        type.innerHTML = name_of_type;
-        type.id = name_of_type;
-        type.className = 'pokemon_type_single';
-        body.insertBefore(type,body.nextSibling);
+function create_types(){ 
+    var total_types = 1; /*Change total_types to size of array containing types*/ 
+    var type_1 = document.getElementById('type_1');
+    var type_2 = document.getElementById('type_2');
+    /** We know we are going to need to set the first type since all pokemon have at least one type */
+    var name_of_type_1 = 'Grass' /*Change this to whatever the element is from the json*/
+    type_1.innerHTML = name_of_type_1;
+    type_1.id = name_of_type_1;
+    if(total_types == 1){
+        type_2.remove();
+        type_1.classList.remove('pokemon_type_dual');
+        type_1.className = 'pokemon_type_single';
     }else{
-        for(var i = 0;i<total_types;i++){
-            var type = document.createElement("button");
-            /**button.className = "btn btn-success";**/
-            type.innerHTML = 'Nope'; /** This is the label on the button*/
-            type.id = name_of_type;
-            type.className = 'pokemon_type_dual';
-            body.insertBefore(type,body.nextSibling);
-        }
+        var name_of_type_2 = 'Grass'
+        type_2.innerHTML =  name_of_type_2;
+        type_2.id = name_of_type_2;
     }
 }
+
 
 function create_accordion(){
     /*Change 5 to whatever the move set # is from the query*/
