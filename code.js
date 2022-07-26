6ad419b3265e5ca3575a418c393706bc953198d9
Julie <jbosworth16@gmail.com>
2022-07-25 21:42:52 -0400

    Updated frontend for adding types

--- code.js
+++ code.js
@@ -1,24 +1,38 @@
-function create_types(){
-    var body = document.getElementsByTagName("body")[0]
-    /*Change 5 to whatever the type # is from the query*/
-    for(var i = 0;i<1;i++){
-        var button = document.createElement("button")
-        button.className = "btn btn-light"
-        button.innerHTML = 'grass' /** This is the label on the button*/
+function create_types(){ /** Need to think how to use this to determine background color maybe read 
+in the text and if = then we choose color? */
+    var body = document.getElementsByTagName("body")[0];
+    /*Change total_types to size of array containing types*/ 
+    var total_types = 1;
+    var name_of_type = 'Grass'
+    if(total_types = 1){
+        var type = document.createElement("button");
+        type.innerHTML = name_of_type;
+        type.id = name_of_type;
+        type.className = 'pokemon_type_single';
+        body.insertBefore(type,body.nextSibling);
+    }else{
+        for(var i = 0;i<total_types;i++){
+            var type = document.createElement("button");
+            /**button.className = "btn btn-success";**/
+            type.innerHTML = 'Nope'; /** This is the label on the button*/
+            type.id = name_of_type;
+            type.className = 'pokemon_type_dual';
+            body.insertBefore(type,body.nextSibling);
+        }
+    }
 }
 
 function create_accordion(){
     /*Change 5 to whatever the move set # is from the query*/
-    var body = document.getElementsByTagName("body")[0]
-    var start = document.getElementById('start')
+    var list = document.getElementsById("pokemon_moves");
     for(var i = 0;i<5;i++){
-        var button = document.createElement("button")
-        button.id = 'accordion'
-        button.className = "btn btn-light"
-        button.innerHTML = 'tackle' /** This is the label on the button*/
-        start.parentNode.insertBefore(button,start)
-        body.appendChild(button)
+        var move = document.createElement("li");
+        move.id = 'accordion';
+        move.appendChild(document.createTextNode("Tackle")); /** This is the label on the button*/
+        document.querySelector('ul').appendChild(move);
+        alert(move);
     }
+    alert(move);
 }
 
 function show_tooltext(){
