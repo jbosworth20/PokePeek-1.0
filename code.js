f36295103d59834cb770665b3d87a081df04b051
Julie <jbosworth16@gmail.com>
2022-07-27 23:27:26 -0400

    Got rid of create_according in favor of create_sliding_list to show moves

--- code.js
+++ code.js
@@ -2,7 +2,6 @@ function create_types(){
     var total_types = 1; /*Change total_types to size of array containing types*/ 
     var type_1 = document.getElementById('type_1');
     var type_2 = document.getElementById('type_2');
-    /** We know we are going to need to set the first type since all pokemon have at least one type */
     var name_of_type_1 = 'Grass' /*Change this to whatever the element is from the json*/
     type_1.innerHTML = name_of_type_1;
     type_1.id = name_of_type_1;
@@ -11,26 +10,29 @@ function create_types(){
         type_1.classList.remove('pokemon_type_dual');
         type_1.className = 'pokemon_type_single';
     }else{
-        var name_of_type_2 = 'Grass'
+        var name_of_type_2 = 'Fairy' /*Change this to whatever the element is from the json*/
         type_2.innerHTML =  name_of_type_2;
         type_2.id = name_of_type_2;
     }
 }
 
 
-function create_accordion(){
+function create_sliding_list(){
     /*Change 5 to whatever the move set # is from the query*/
-    var list = document.getElementsById("pokemon_moves");
-    for(var i = 0;i<5;i++){
-        var move = document.createElement("li");
-        move.id = 'accordion';
-        move.appendChild(document.createTextNode("Tackle")); /** This is the label on the button*/
-        document.querySelector('ul').appendChild(move);
-        alert(move);
+    var num_moves = 20;
+    for(var i = 0;i<num_moves;i++){
+        var move = document.createElement("p");
+        var move_name = document.createTextNode("Test??"); /** Replace this with move_name */
+        move.append(move_name);
+        document.getElementsById("begin").appendChild(move,list);
     }
-    alert(move);
 }
-
+/** For function below was thinking this - go and get move from json that matches the text and from there auto populate */
+function show_move(){
+    var p = document.createElement
+    document.appendChild("p");
+    p.innerHTML = "HelloF"
+}
 function show_tooltext(){
 
 }
