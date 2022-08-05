f56876415c594a0eac45c82129fd55616a7285ab
Julie <jbosworth16@gmail.com>
2022-08-04 23:28:09 -0400

    Added Flask support

--- api.py
+++ api.py
@@ -1,6 +1,12 @@
-import flask
+from flask import Flask, render_template
 import requests
-
+#Flask
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
 #Get Pokemon
 pokemon_name = "bulbasaur"
 if pokemon_name.lower() == "mimikyu":
