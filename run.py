de15289f3da96923dcb74cae0de284da9ab262ab
Julie <jbosworth16@gmail.com>
2022-08-09 19:09:56 -0400

    Reformatted Run to no longer need prefix

--- run.py
+++ run.py
@@ -1,26 +1,26 @@
-import flask
+from flask import *
 from api import *
 #Flask
-app = flask.Flask(__name__)
+app = Flask(__name__)
 #The homepage
 @app.route('/')
 def home():
-    return flask.render_template('index.html')
+    return render_template('home.html')
 #What goes on when the user clicks the search button
 @app.route("/search", methods = ['POST', 'GET'])
 def search():
-    if flask.request.method == "POST":
-        pokemon_name = flask.request.form['search']
+    if request.method == "POST":
+        pokemon_name = request.form['search']
         pokemon = api(pokemon_name)
         pokemon_info = pokemon.get_pokemon()
         pokemon_name = pokemon.get_name(pokemon_info)
-        return flask.redirect(flask.url_for('pokemon',name = pokemon_name))
-    return flask.render_template('index.html')
+        return redirect(url_for('pokemon',name = pokemon_name))
+    return render_template('home.html')
 
 #The pokemon name route for the search
 @app.route("/pokemon/<name>")
 def pokemon(name):
-    return flask.render_template('pokemon.html',name = name)
+    return render_template('pokemon.html',name = name)
 if __name__ == '__main__':
     app.run(debug = True)
 
