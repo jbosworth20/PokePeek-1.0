4a789399f9e89be4377747f9db678b339a91c8d6
Julie <jbosworth16@gmail.com>
2022-08-09 18:49:02 -0400

    Renamed API & made it so api and run are connected

--- run.py
+++ run.py
@@ -1,24 +1,26 @@
-from flask import *
+import flask
 from api import *
-import requests
 #Flask
-app = Flask(__name__)
+app = flask.Flask(__name__)
 #The homepage
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return flask.render_template('index.html')
 #What goes on when the user clicks the search button
 @app.route("/search", methods = ['POST', 'GET'])
 def search():
-    if request.method == "POST":
-        pokemon_name = request.form['name']
-        return redirect(url_for('pokemon',name = pokemon_name))
-    return render_template('index.html')
+    if flask.request.method == "POST":
+        pokemon_name = flask.request.form['name']
+        pokemon = api(pokemon_name)
+        pokemon_info = pokemon.get_pokemon()
+        pokemon_name = pokemon.get_name(pokemon_info)
+        return flask.redirect(flask.url_for('pokemon',name = pokemon_name))
+    return flask.render_template('index.html')
 
 #The pokemon name route for the search
 @app.route("/pokemon/<name>")
 def pokemon(name):
-    return render_template('pokemon.html',name = name)
+    return flask.render_template('pokemon.html',name = name)
 if __name__ == '__main__':
     app.run(debug = True)
 
