b495fdd9aea6fd8da9befe210a80120edd17ddbe
Julie <jbosworth16@gmail.com>
2022-08-08 22:07:35 -0400

    Turned the original API class to run class

---
+++ run.py
@@ -0,0 +1,23 @@
+from flask import *
+import requests
+#Flask
+app = Flask(__name__)
+#The homepage
+@app.route('/')
+def home():
+    return render_template('index.html')
+#What goes on when the user clicks the search button
+@app.route("/search", methods = ['POST', 'GET'])
+def search():
+    if request.method == "POST":
+        pokemon_name = request.form['name']
+        return redirect(url_for('pokemon',name = pokemon_name))
+    return render_template('index.html')
+
+#The pokemon name route for the search
+@app.route("/pokemon/<name>")
+def pokemon(name):
+    return render_template('pokemon.html',name = name)
+if __name__ == '__main__':
+    app.run(debug = True)
+
