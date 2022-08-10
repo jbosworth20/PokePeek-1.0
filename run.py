dfa76357fdf8dfc12f95d589765b783257dd7eeb
Julie <jbosworth16@gmail.com>
2022-08-09 20:56:33 -0400

    added code to handle if not a valid pokemon name

--- run.py
+++ run.py
@@ -10,17 +10,32 @@ def home():
 @app.route("/search", methods = ['POST', 'GET'])
 def search():
     if request.method == "POST":
-        pokemon_name = request.form['search']
-        pokemon = api(pokemon_name)
-        pokemon_info = pokemon.get_pokemon()
-        pokemon_name = pokemon.get_name(pokemon_info)
-        return redirect(url_for('pokemon',name = pokemon_name))
+        try:
+            pokemon_name = request.form['search']
+            pokemon = api(pokemon_name)
+            pokemon_info = pokemon.get_pokemon()
+            pokemon_name = pokemon.get_name(pokemon_info)
+            return redirect(url_for('pokemon',name = pokemon_name))
+        except:
+            return redirect(url_for('error'))
     return render_template('home.html')
 
 #The pokemon name route for the search
 @app.route("/pokemon/<name>")
 def pokemon(name):
     return render_template('pokemon.html',name = name)
+
+
+@app.route("/error",  methods = ['POST', 'GET'])
+def error():
+    try:
+        pokemon_name = request.form['search']
+        pokemon = api(pokemon_name)
+        pokemon_info = pokemon.get_pokemon()
+        pokemon_name = pokemon.get_name(pokemon_info)
+        return redirect(url_for('pokemon',name = pokemon_name))
+    except:
+        return render_template("error.html")
+
 if __name__ == '__main__':
     app.run(debug = True)
-
