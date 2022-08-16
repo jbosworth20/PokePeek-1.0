7ecf52a095eb47ed8e7615d0c2706913c6ef225c
Julie <jbosworth16@gmail.com>
2022-08-16 14:03:09 -0400

    Make things easier for move creation

--- static/code.js
+++ static/code.js
@@ -6,15 +6,6 @@ function unhide(class_to_unhide){ /** Make this so it so that there is a class w
         element.style.visibility = "hidden";
     }
 }
-function create_sliding_list(){
-    /*Change 5 to whatever the move set # is from the query*/
-    var num_moves = 20;
-    for(var i = 0;i<num_moves;i++){
-        var move = document.createElement("p");
-        var move_name = document.createTextNode("Test??"); /** Replace this with move_name */
-        move.append(move_name);
-        document.getElementsById("scrollbox").appendChild(move,list);
-    }
-}
+
 
 
