2f9bc87f92d8d71a5c8c44868d43e73ff73af797
Julie <jbosworth16@gmail.com>
2022-08-15 22:50:56 -0400

    Make it so name was dynamic

--- run.py
+++ run.py
@@ -2,28 +2,38 @@ from flask import *
 from api import *
 #Flask
 app = Flask(__name__)
+pokemon_info = []
 #The homepage
 @app.route('/')
 def home():
     return render_template('home.html')
 #What goes on when the user clicks the search button
 @app.route("/search", methods = ['POST', 'GET'])
-def search():
-    if request.method == "POST":
-        try:
-            pokemon_name = request.form['search']
-            pokemon = api(pokemon_name)
-            pokemon_info = pokemon.get_pokemon()
-            pokemon_name = pokemon.get_name(pokemon_info)
+def search(): #This method is terrible - make it better in terms of making it simple and less weird
+    if request.method == "POST": 
+        pokemon_name = request.form['search']
+        pokemon = api(pokemon_name)
+        json_ = pokemon.get_pokemon()
+        pokemon_name = pokemon.get_name(json_)
+        if(pokemon.confirm_legit(json_) != '404'):
             return redirect(url_for('pokemon',name = pokemon_name))
-        except:
+        else:
             return redirect(url_for('error'))
     return render_template('home.html')
 
 #The pokemon name route for the search
 @app.route("/pokemon/<name>")
-def pokemon(name):
-    return render_template('pokemon.html',name = name)
+def pokemon(name): #Make the pokemon api parts better
+    api_ = api(name)
+    json = api_.get_pokemon()
+    pokemon_info = []
+    pokemon_name = api_.get_name(json)
+    pokemon_type = api_.get_types(json)
+    pokemon_stats = api_.get_stats(json)
+    pokemon_abilities = api_.get_abilities(json)
+    #pokemon_moves = api_.get_moves(json) <- Get this to work
+    pokemon_info.extend([pokemon_name,pokemon_type,pokemon_stats,pokemon_abilities])
+    return render_template('pokemon.html',name = pokemon_info[0],data = pokemon_info) #issue is with html not this
 
 
 @app.route("/error",  methods = ['POST', 'GET'])
