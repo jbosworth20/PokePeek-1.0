a019958d60e920048dba6a823c0b828cb50d13d4
Julie <jbosworth16@gmail.com>
2022-08-17 19:00:08 -0400

    Changed class to hide to id to hide

--- static/code.js
+++ static/code.js
@@ -1,5 +1,5 @@
-function unhide(class_to_unhide){ /** Make this so it so that there is a class we call and then based on individual ID we determine what is visible */
-    var element = document.getElementByClassName(class_to_unhide);
+function unhide(id_to_unhide){ /** Make this so it so that there is a class we call and then based on individual ID we determine what is visible */
+    var element = document.getElementById(id_to_unhide);
     if(element.style.visibility == "hidden"){
         element.style.visibility = "visible";
     }else{
