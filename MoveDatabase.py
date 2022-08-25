ea10b34fbea24bb07d1a8ef72a3a22afab0c34cc
Julie <jbosworth16@gmail.com>
2022-08-25 16:13:54 -0400

    Reverted changes

--- MoveDatabase.py
+++ MoveDatabase.py
@@ -9,27 +9,6 @@ from pandas import *
 class MoveDataBase:
     def __init__(self,pokemon_name):
         self.pokemon_name = pokemon_name
-    def get_info(self,move):
-        move_info = []
-        name = move
-        move_info.append(name)
-        link = "https://pokeapi.co/api/v2/move/" + name
-        move_response = requests.get(link).json()
-        #Description
-        description = move_response.get("effect_entries")[0].get('effect')
-        move_info.append(description)
-        #Accuracy
-        accuracy = move_response.get("accuracy")
-        move_info.append(accuracy)
-        #Power
-        power = move_response.get("power")
-        move_info.append(power)
-        #PP
-        pp = move_response.get("pp")
-        move_info.append(pp)
-        #Type
-        type_ = move_response.get("type").get("name")
-        move_info.append(type_)
     def insert_moves(self): #Try to get name from pokemon page and then get individual move info on click and make it a tuple
         metadata = MetaData()
         moves = Table('moves',metadata,
@@ -46,8 +25,19 @@ class MoveDataBase:
         move_json = pokemon_info["moves"]
         for i, move in enumerate(move_json,start = 0):
             name = move_json[i].get("move").get("name")
-            move_info = self.get_info(name)
-            #insert(moves).values(move_name = name,description = description,accuracy = accuracy,power = power,pp = pp,type = type_)
+            link = "https://pokeapi.co/api/v2/move/" + name
+            move_response = requests.get(link).json()
+            #Description
+            description = move_response.get("effect_entries")[0].get('effect')
+            #Accuracy
+            accuracy = move_response.get("accuracy")
+            #Power
+            power = move_response.get("power")
+            #PP
+            pp = move_response.get("pp")
+            #Type
+            type_ = move_response.get("type").get("name")
+            insert(moves).values(move_name = name,description = description,accuracy = accuracy,power = power,pp = pp,type = type_)
         dataframe = read_sql_table('moves',engine) 
         return dataframe
     
