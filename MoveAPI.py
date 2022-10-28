5d18d2b39cc383d5371f246bc399ace906fb30ae
jbosworth16 <jbosworth16@gmail.com>
2022-10-28 11:14:22 -0400

    Added format_accuracy for formatting

--- MoveAPI.py
+++ MoveAPI.py
@@ -3,7 +3,12 @@ from flask import *
 class MoveAPI:
     def __init__(self,move_name):
             self.move_name = move_name
-    
+    def format_accuracy(self,accuracy):
+        accuracy = str(accuracy)
+        if(accuracy.isdigit()):
+            accuracy = accuracy + "%"
+            return accuracy
+        return accuracy
     def get_move_info(self): 
         link = "https://pokeapi.co/api/v2/move/" + self.move_name #issue is with move_name is literally move_name
         print(link)
@@ -12,7 +17,7 @@ class MoveAPI:
         move_info = {
             "name":  self.move_name,
             "description": json.get("effect_entries")[0].get('effect'),
-            "accuracy": json.get("accuracy"),
+            "accuracy": self.format_accuracy(json.get("accuracy")),
             "power": json.get("power"),
             "pp": json.get("pp"),
             "type": json.get("type").get("name")
