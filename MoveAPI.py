6fdd58b21fd449e086ce25a04cead8afffbb66da
jbosworth16 <jbosworth16@gmail.com>
2022-12-11 17:35:53 -0500

    Added function to show effect chance

--- MoveAPI.py
+++ MoveAPI.py
@@ -16,10 +16,15 @@ class MoveAPI:
         json = move_response.json()
         move_info = {
             "name":  self.move_name,
-            "description": json.get("effect_entries")[0].get('effect'),
+            "description": self.format_desc(json,json.get("effect_entries")[0].get('effect')),
             "accuracy": self.format_accuracy(json.get("accuracy")),
             "power": json.get("power"),
             "pp": json.get("pp"),
             "type": json.get("type").get("name")
             }
         return move_info
+    
+    def format_desc(self,json,desc):
+        desc = str(desc)
+        new_desc = desc.replace("$effect_chance",str(json.get("effect_chance")))
+        return new_desc
